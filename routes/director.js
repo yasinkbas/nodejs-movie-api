bf7f1ca937b50240831808e5d2b652d272a0ac84
yasinkbas <yasin.kbas12@gmail.com>
2018-10-10 23:46:38 +0300

    :director_id get

--- routes/director.js
+++ routes/director.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose')
 const express = require('express');
 const router = express.Router();
 
@@ -91,6 +92,84 @@ router.get('/',(req,res)=>{
         res.json(err)
     })
 })
+
+router.get('/:director_id',(req,res)=>{
+    const promise = Director.aggregate([
+        /**
+         * we want to only person data in here
+         * but this returns object we resolve this problem with used a mongoose's method Types.ObjectId
+         */
+        {
+            $match: {
+                '_id': mongoose.Types.ObjectId(req.params.director_id)
+            }
+        },
+        /**
+         * for join task
+         * this directorId in Directors will matches _id in Movies
+         * and this will assign to movies variable
+         */
+        {
+            $lookup: {
+                from: 'moviews',
+                localField: '_id',
+                foreignField: 'director_id',
+                as: 'movies'
+            }
+        },
+        /**
+         * we need the data use below that 
+         * we need to attribute this unwind
+         * @param preserveNullAndEmptyArrays: herhangi bir data ile eslesmese bile 
+         * o directoru gondermesini saglar
+         */
+        {
+            $unwind: {
+                path : '$movies',
+                preserveNullAndEmptyArrays:true
+            }
+        },
+        /**
+         * we need to group them
+         * because one director can have two movie but these will be shown separately
+         * but we want to these shown with groups under the one person
+         */
+        {
+            $group:{
+                _id : {
+                    _id : '$_id',
+                    name: '$name',
+                    surname: '$surname',
+                    bio: '$bio'
+                },
+                /**
+                 * we need the push this group to under the movies param
+                 */
+                movies: {
+                    $push: '$movies'
+                }
+            }
+        },
+        {
+            /**
+             * we weant to delete super.id because we have two id 
+             * we assigned these to id
+             */
+            $project: {
+                _id : '$_id._id',
+                name: '$_id.name',
+                surname: '$_id_surname',
+                movies: '$movies'
+            }
+        }
+    ]);
+    promise.then((data)=>{
+        res.json(data)
+    }).catch((err)=>{
+        res.json(err)
+    })
+})
+
 
 
 
