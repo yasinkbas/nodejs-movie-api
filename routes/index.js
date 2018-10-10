292e12d20bb09ea6b2fd79c0401992e999b4f078
yasinkbas <yasin.kbas12@gmail.com>
2018-10-11 01:21:56 +0300

    added bcrypt & edited register

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt')
 
 //Models
 const User = require('../models/User')
@@ -17,19 +18,31 @@ router.post('/register', function(req, res, next) {
   const {username, password} = req.body
 
   /**
-   * this mean is username = username, password = password
+   * this is bcrypt module
+   * enctypting password
+   */
+  bcrypt.hash(password,10).then((hash) =>{
+    // Store hash in your password DB
+
+  /**
+   * old :this mean is username = username, password = password
+   * new : hash assigned to password
    * this comed with ecmascript
    */
-  const user = new User({
-    username,
-    password
+    const user = new User({
+      username,
+      password: hash
+    })
+    const promise = user.save()
+    promise.then((data) =>{
+      res.json(data)
+    }).catch((err)=>{
+      res.json(err)
+    })
   })
-  const promise = user.save()
-  promise.then((data) =>{
-    res.json(data)
-  }).catch((err)=>{
-    res.json(err)
-  })
+
+  
+  
 });
 
 module.exports = router;
