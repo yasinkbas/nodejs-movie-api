ad015e75e9759e85e7c1c6a9fe55b8856bdcc921
yasinkbas <yasin.kbas12@gmail.com>
2018-10-12 02:59:36 +0300

    test movie post/

--- test/api/movie.test.js
+++ test/api/movie.test.js
@@ -32,5 +32,33 @@ describe('/api/movies tests', () => {
         })
     })
 
+    describe('/POST movie',()=>{
+        it('it should post a movie', (done)=>{
+            const movie = {
+                title: "Udemy",
+                director_id: '5bbe597062041109ac2ceefa',
+                category: 'Komedi',
+                country: 'Turkey',
+                year: '1950',
+                imdb_score: 8
+            }
+            chai.request(server)
+            .post('/api/movies')
+            .send(movie)
+            .set('x-access-token',token)
+            .end((err,res)=>{
+                res.should.have.status(200)
+                res.body.should.be.a('object')
+                res.body.should.have.property('title')
+                res.body.should.have.property('director_id')
+                res.body.should.have.property('category')
+                res.body.should.have.property('country')
+                res.body.should.have.property('year')
+                res.body.should.have.property('imdb_score')
 
+                done()
+
+            })
+        })
+    })
 })
