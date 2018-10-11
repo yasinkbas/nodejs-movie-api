71b72cbebbe5b8c54a91f4d12111c9c947387b74
yasinkbas <yasin.kbas12@gmail.com>
2018-10-11 23:32:23 +0300

    created npm test script

--- test/api/index.test.js
+++ test/api/index.test.js
@@ -1,7 +1,7 @@
 const chai = require('chai')
 const chaiHttp = require('chai-http')
 const sohuld = chai.should()
-const server = require('../app')
+const server = require('../../app')
 
 chai.use(chaiHttp)
 
