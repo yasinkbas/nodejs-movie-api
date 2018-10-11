6928cf6e6c678f2bb199fcfb584fc7474a6169c3
yasinkbas <yasin.kbas12@gmail.com>
2018-10-11 23:25:09 +0300

    installed chai & chai-http & mocha

---
+++ test/index.test.js
@@ -0,0 +1,17 @@
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const sohuld = chai.should()
+const server = require('../app')
+
+chai.use(chaiHttp)
+
+describe('Node Server', () => {
+    it('(GET /) anasayfayi dondurur', (done) =>{
+        chai.request(server)
+        .get('/')
+        .end((err,res) =>{
+            res.should.have.status(200)
+            done()
+        })
+    })
+})
