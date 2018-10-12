5088055614fc87bc3dce6962b444b3f0333f4f5b
yasinkbas <yasin.kbas12@gmail.com>
2018-10-13 01:20:32 +0300

    added all of test

--- test/api/movie.test.js
+++ test/api/movie.test.js
@@ -55,10 +55,75 @@ describe('/api/movies tests', () => {
                 res.body.should.have.property('country')
                 res.body.should.have.property('year')
                 res.body.should.have.property('imdb_score')
-
+                movieId = res.body._id;
                 done()
 
             })
         })
     })
+
+    describe('/GET/:movie_id movie', () => {
+		it('it should GET a movie by the given id', (done) => {
+			chai.request(server)
+				.get('/api/movies/' + movieId)
+				.set('x-access-token', token)
+				.end((err, res) => {
+					res.should.have.status(200);
+					res.body.should.be.a('object');
+					res.body.should.have.property('title');
+					res.body.should.have.property('director_id');
+					res.body.should.have.property('category');
+					res.body.should.have.property('country');
+					res.body.should.have.property('year');
+					res.body.should.have.property('imdb_score');
+                    res.body.should.have.property('_id').eql(movieId);
+                    
+					done();
+				});
+		});
+	});
+
+	describe('/PUT/:movie_id movie', () => {
+		it('it should UPDATE a movie given by id', (done) => {
+			const movie = {
+				title: '93creative',
+				director_id: '5a34e1afb8523a78631f8541',
+				category: 'Suç',
+				country: 'Fransa',
+				year: 1970,
+				imdb_score: 9
+			};
+
+			chai.request(server)
+				.put('/api/movies/' + movieId)
+				.send(movie)
+				.set('x-access-token', token)
+				.end((err, res) => {
+					res.should.have.status(200);
+					res.body.should.be.a('object');
+					res.body.should.have.property('title').eql(movie.title);
+					res.body.should.have.property('director_id').eql(movie.director_id);
+					res.body.should.have.property('category').eql(movie.category);
+					res.body.should.have.property('country').eql(movie.country);
+					res.body.should.have.property('year').eql(movie.year);
+					res.body.should.have.property('imdb_score').eql(movie.imdb_score);
+
+					done();
+				});
+		});
+	});
+
+	describe('/DELETE/:movie_id movie', () => {
+		it('it should DELETE a movie given by id', (done) => {
+			chai.request(server)
+				.delete('/api/movies/' + movieId)
+				.set('x-access-token', token)
+				.end((err, res) => {
+					res.should.have.status(200);
+					res.body.should.be.a('object');
+					res.body.should.have.property('status').eql(1);
+					done();
+				});
+		});
+	});
 })
